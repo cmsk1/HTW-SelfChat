1a44e36c34d5f3e1e65ad9219b8a5f740e826e8c
cs070 <constantin.schulte-kersmecke@rbb-online.de>
2022-01-24 10:34:11 +0100

    Initial Commit

---
+++ SelfChat_572847/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SelfChat_572847
+//
+//  Created by Constantin Schulte-Kersmecke on 24.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SelfChat_572847/SelfChat_572847App.swift
@@ -0,0 +1,17 @@
+//
+//  SelfChat_572847App.swift
+//  SelfChat_572847
+//
+//  Created by Constantin Schulte-Kersmecke on 24.01.22.
+//
+
+import SwiftUI
+
+@main
+struct SelfChat_572847App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
